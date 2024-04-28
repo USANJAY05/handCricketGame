dab06b83a5adeefae5e08d4ea59d8affaa8b99bf
Sanjay Sanjay <sanjaysanjay@sanjay15.local>
2024-04-28 11:47:51 +0530

    changed state value

--- src/App.js
+++ src/App.js
@@ -5,19 +5,47 @@ import Main from './Main'
 const App = () => {
   const [yScore,setYScore]=useState(0)
   const [mScore,setMScore]=useState(0)
-  const [yPoint,setYPoint]=useState()
-  const [mPoint,setMPoint]=useState()
-  const [batBall,setBatBall]=useState("Hand Cricket")
+  const [yPoint,setYPoint]=useState(0)
+  const [mPoint,setMPoint]=useState(0)
+  const [batBall,setBatBall]=useState("Batting")
+  const [num,setNum]=useState(0)
 
   const handleInput=(val)=>{
     const value=parseInt(val)
     const randomNumber = Math.floor(Math.random() * 7);
-    console.log(randomNumber);
-    const score=value+parseInt(yScore)
-    setYScore(score)
-    setMPoint(randomNumber)
-    setYPoint(value)
-    console.log(value)
+    if(value===randomNumber){
+      setNum(1)
+    }
+    else if(num===0){
+      const score=value+yScore
+      setYScore(score)
+      setMPoint(randomNumber)
+      setYPoint(value)
+    }
+    else if(num===1){
+      setBatBall("Bowling")
+      const score=randomNumber+mScore
+      setMScore(score)
+      setMPoint(randomNumber)
+      setYPoint(value)
+      if(num===randomNumber || value<randomNumber){
+        setNum(2)
+      }
+    }
+    else{
+      if(mScore==="YOU LOSE" || mScore==="YOU WON"){
+        console.log("HI")
+      }
+      else if(mScore>yScore){
+        setMScore("YOU LOSE")
+        setYScore("YOU LOSE")
+      }
+      else{
+        setYScore("YOU WON")
+        setMScore("YOU WON")
+      }
+    }
+
   }
   return (
     <section className='app'>
