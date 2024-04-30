3237401ffa19a6538dbb64c9647f90223e8c57ff
Sanjay Sanjay <sanjaysanjay@sanjay15.local>
2024-04-30 17:04:36 +0530

    added alert box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './App.css'
 import Main from './Main'
+import AlertBox from './AlertBox'
 
 const App = () => {
   const [yScore,setYScore]=useState(0)
@@ -8,48 +9,98 @@ const App = () => {
   const [yPoint,setYPoint]=useState(0)
   const [mPoint,setMPoint]=useState(0)
   const [batBall,setBatBall]=useState("Batting")
+  const [bb,setBB]=useState(0)
   const [num,setNum]=useState(0)
+  const [alert,setAlert]=useState("none")
+  const [msg,setMsg]=useState()
+  let [count,setCount]=useState(0)
+
+
+  const handleReset = () => {
+    window.location.reload()
+  };
+  
+  
 
   const handleInput=(val)=>{
     const value=parseInt(val)
     const randomNumber = Math.floor(Math.random() * 7);
-    if(value===randomNumber){
-      setNum(1)
-    }
-    else if(num===0){
+    setMPoint(randomNumber)
+    setYPoint(value)
+    console.log(randomNumber)
+
+    const you=()=>{
+      setBatBall("Batting")
       const score=value+yScore
       setYScore(score)
-      setMPoint(randomNumber)
-      setYPoint(value)
     }
-    else if(num===1){
-      setBatBall("Bowling")
+    const machine=()=>{
       const score=randomNumber+mScore
       setMScore(score)
-      setMPoint(randomNumber)
-      setYPoint(value)
-      if(num===randomNumber || value<randomNumber){
-        setNum(2)
-      }
     }
-    else{
-      if(mScore==="YOU LOSE" || mScore==="YOU WON"){
-        console.log("HI")
-      }
-      else if(mScore>yScore){
-        setMScore("YOU LOSE")
-        setYScore("YOU LOSE")
+    (bb===0)?setBatBall("Batting"):setBatBall("Bownling")
+    
+
+    console.log(count)
+
+    if((count>1 && ((yScore>1)&&(mScore>1)))){
+      setAlert(true)
+      if(mScore>yScore){
+        setMsg("YOU LOSS")
       }
       else{
-        setYScore("YOU WON")
-        setMScore("YOU WON")
+        setMsg("YOU Won")
       }
+      console.log("error")
     }
+    else if(value===randomNumber && count<=2){
+      setBB(!bb)
+      count+=1
+      setCount(count)
+    }
+    else if(bb===0){
+      you()
+    }
+    else if(bb===1){
+      machine()
+    }
+    else{
+      
+    }
+    // if(value===randomNumber){
+    //   setNum(1)
+    // }
+    // else if(num===0){
+    //   machine()
+    // }
+    // else if(num===1){
+    //   you()
+    //   if(num===randomNumber || value<randomNumber){
+    //     setNum(2)
+    //   }
+    // }
+    // else{
+    //   if(mScore==="YOU LOSE" || mScore==="YOU WON"){
+    //     console.log("HI")
+    //   }
+    //   else if(mScore>yScore){
+    //     setAlert(true)
+    //     setMsg("You Lose")
+    //   }
+    //   else{
+    //     setAlert(true)
+    //     setMsg("You won")
+    //   }
+    // }
+
+
+
 
   }
   return (
     <section className='app'>
       <Main handleInput={handleInput} yScore={yScore} mScore={mScore} yPoint={yPoint} mPoint={mPoint} batBall={batBall} />
+      <AlertBox alert={alert} msg={msg} handleReset={handleReset}/>
     </section>
   )
 }
